fa5718130e34b95753292514917922a649f41a5a
jclydemar <clydemar234@gmail.com>
2023-02-05 21:31:42 +0800

    Add project files.

---
+++ PracticeLang/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+using System;
+
+namespace PracticeLang
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
